e10d3184525ca4356b8121638fe0be9444bc4a1e
Lakshmibai Nyamagoudra <laknymam@WKMZT15EACCC.global.publicisgroupe.net>
2025-07-01 12:55:30 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,33 @@
+//  <div id="parent">
+//      <div id="child">
+//          <h1></h1>
+// </div>
+// </div>
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    {
+      id: "child",
+    },
+    [
+      React.createElement("h1", { key: "h1" }, "This is h1 tag!"),
+      React.createElement("h2", { key: "h2" }, "This is h2 tag!"),
+    ]
+  )
+);
+
+// react is creating core h1 tag
+// react element(object) => HTMl(browser understands)
+const heading = React.createElement(
+  "h1",
+  { id: "heading", className: "heading" },
+  "Hello World from react!"
+);
+// created a root for react application
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// this will replace the code in the root
+// library - it can make small portion of the app
+root.render(parent);
